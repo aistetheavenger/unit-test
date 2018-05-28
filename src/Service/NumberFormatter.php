ddc9cd61306017df7265b6892f6d8517691805bf
Aiste Jurevice <project.blon@gmail.com>
2018-05-28 21:11:05 +0300

    Update NumberFormatter.php

--- src/Service/NumberFormatter.php
+++ src/Service/NumberFormatter.php
@@ -10,21 +10,20 @@ class NumberFormatter
             $number = number_format((float)$number/1000000, 2, '.', '').'M';
         }
 
-        if(abs($number) < 999500 && abs($number) >= 99950)
+        elseif(abs($number) < 999500 && abs($number) >= 99950)
         {
             $number = round($number/1000).'K';
         }
 
-        if(abs($number) < 99950 && abs($number) >= 1000)
+        elseif(abs($number) < 99950 && abs($number) >= 1000)
         {
             $number = number_format((float)$number, 0, '.', ' ');
         }
 
-        if(abs($number) >= 0 && abs($number) < 1000)
+        elseif(abs($number) >= 0 && abs($number) < 1000)
         {
             $number = round($number, 2);
         }
-
         	return $number;
     }
 }
